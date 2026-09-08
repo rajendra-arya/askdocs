f3dc45e0eddbbcacbad7f0940175577069e9a84c
Rajendra Arya <rajendra.official@hotmail.com>
2026-09-08 14:34:35 +0530

    feat: add source citations to RAG responses

--- loaders/qdrant_store.py
+++ loaders/qdrant_store.py
@@ -1,25 +1,23 @@
-import pymupdf4llm
+import pymupdf
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, PointStruct, VectorParams
 
 from .embedder import create_embeddings
 from .pdf_loader import make_chunks
 
-## intialize connection with client
+# intialize connection with client
 client = QdrantClient(url="http://localhost:6333")
 
 # create collection
 if not client.collection_exists("askdocs"):
     # load pdf
-    doc_text = pymupdf4llm.to_text("./data/The Accidental CTO Book.pdf")
+    doc = pymupdf.open("./data/The Accidental CTO Book.pdf")
 
     # create chunks
-    text_chunks = make_chunks(
-        data=doc_text, chunk_size=500, chunk_overlap=100, min_chunk_size=250
-    )
+    chunks = make_chunks(doc=doc, chunk_size=500, chunk_overlap=100, min_chunk_size=250)
 
-    # create embeddings
-    embeddings = create_embeddings(data=text_chunks)
+    # create embeddings for chunk text only
+    embeddings = create_embeddings(data=[i["chunk"] for i in chunks])
 
     # store embedddings in qdrant
     ## create collection
@@ -28,18 +26,22 @@ if not client.collection_exists("askdocs"):
         vectors_config=VectorParams(size=384, distance=Distance.COSINE),
     )
 
-    ## upsert doc chunk embeddings to qdrant collection
+    ## upsert doc chunk embeddings and metadata to qdrant collection
     info = client.upsert(
         collection_name="askdocs",
         points=[
-            PointStruct(id=rank, vector=vector, payload={"text": text})
-            for rank, (vector, text) in enumerate(zip(embeddings, text_chunks), start=1)
+            PointStruct(
+                id=rank,
+                vector=vector,
+                payload={"text": chunk["chunk"], "page_no": chunk["page_no"]},
+            )
+            for rank, (vector, chunk) in enumerate(zip(embeddings, chunks), start=1)
         ],
     )
     print(info)
 
 
-# get chunks
+# get chunks and metadata
 def retrieve_chunks(query):
     query_embedding = create_embeddings(query)
     # search
@@ -47,7 +49,18 @@ def retrieve_chunks(query):
         collection_name="askdocs", query=query_embedding, with_payload=True, limit=2
     ).points
     if search_result:
-        data = [{"score":point.score,"text":point.payload["text"]} for point in search_result]
+        data = [
+            {
+                "score": point.score,
+                "text": point.payload["text"],
+                "page_no": point.payload["page_no"],
+            }
+            for point in search_result
+        ]
         return data
     else:
         return []
+
+
+# delete collection
+# client.delete_collection("askdocs")
